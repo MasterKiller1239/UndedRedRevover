315d065fed29664d5ef67054cfcd1936d50e6f8f
jedrak <jedrak32@gmail.com>
2019-10-30 13:36:29 +0100

    Fixed timer, it now adds bullets if time is increasing

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,22 +10,50 @@ public class Timer : MonoBehaviour
     private List<GameObject> _listOfBullets;
     private float _currentTime;
     
-    // Start is called before the first frame update
+
     void Start()
     {
+        float trans = 0;
         _listOfBullets = new List<GameObject>();
         for(int i = 0; i < timeToEnd/10; i++)
         {
-            Debug.Log(i);
-            GameObject bullet = Instantiate(bulletPrefab, new Vector3(i * .1F, 0, 0), Quaternion.Euler(0, 0, 90));
+            if(i % 6 != 0)
+            {
+                trans += .1f;
+            }  
+            else
+            {
+                trans += .2f;
+            }
+            GameObject bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x + trans, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 90));
             _listOfBullets.Add(bullet);
         }
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         _currentTime += Time.fixedDeltaTime;
+        if(((timeToEnd - _currentTime) / 10)+1 > _listOfBullets.Count){
+            float trans = 0;
+            while(((timeToEnd - _currentTime) / 10)+1 > _listOfBullets.Count){
+                if(_listOfBullets.Count % 6 != 0)
+                {
+                    trans = .1f;
+                }
+                else
+                {
+                    trans = .2f;
+                }
+                GameObject bullet = Instantiate(bulletPrefab,
+                new Vector3(_listOfBullets[_listOfBullets.Count-1].transform.position.x + trans,
+                    _listOfBullets[_listOfBullets.Count-1].transform.position.y,
+                    _listOfBullets[_listOfBullets.Count-1].transform.position.z),
+                    Quaternion.Euler(0, 0, 90));
+                    _listOfBullets.Add(bullet);
+                }
+                
+        }
         if(((timeToEnd - _currentTime) / 10)+1 < _listOfBullets.Count)
         {
             Destroy(_listOfBullets[_listOfBullets.Count - 1]);
